f5c10f6626fd43bb08b747f98e4a8d80cba9b2c9
aditya verma <piyushxvrm05@gmail.com>
2026-07-23 11:54:28 +0530

    initial commit

---
+++ Lab1/hello.js
@@ -0,0 +1,12 @@
+console.log("Aditya Verma")
+
+// write a functions to take two numbers as input and return their sum
+function sum(a, b) {
+    return a + b;
+}
+// or
+const add2 = (a, b) => a + b;
+
+console.log(sum(5, 10));
+console.log(sum(5, 1));
+console.log(sum(5, 8));
